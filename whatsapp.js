1ce654789fa5a66956b0d5f5af6768b9836788de
BLACKKING011 <michealemenalom5@gmail.com>
2025-03-27 21:25:11 +0100

    Add files via upload

---
+++ whatsapp.js
@@ -0,0 +1,9 @@
+document.querySelector('.join-btn').addEventListener('click', function() {
+    alert('You are being redirected to WhatsApp...');
+});
+document.querySelector('.join-btn').addEventListener('click', function() {
+    window.location.href = 'https://wa.me/2348100000000'; // Replace with your WhatsApp number
+});
+document.querySelector('.join-btn').addEventListener('click', function() {
+    alert('You are being redirected to WhatsApp...');
+});
